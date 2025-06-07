4434ae5b7620daece81a30859cbd5973ecd5d26c
Cybernetics <mail@cybernetics.me>
2025-06-07 09:45:28 +0530

    small updates

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,19 @@
+<!-- Mobile Menu -->
+<div class="mobile-nav-wrapper" role="navigation">
+    <div class="closemobileMenu"><i class="icon an an-times-circle closemenu"></i> Close Menu</div>
+    <ul id="MobileNav" class="mobile-nav">
+        <li class="lvl1 parent megamenu">
+            <a href="{{ route('index') }}">HOME <i class="an an-plus"></i></a>
+        </li>
+        <li class="lvl1 parent megamenu">
+            <a href="{{ route('index') }}">ABOUT US <i class="an an-plus"></i></a>
+        </li>
+        <li class="lvl1 parent megamenu">
+            <a href="{{ route('index') }}">PRODUCTS <i class="an an-plus"></i></a>
+        </li>
+        <li class="lvl1 parent megamenu">
+            <a href="{{ route('index') }}">CONTACT US <i class="an an-plus"></i></a>
+        </li>
+    </ul>
+</div>
+<!-- End Mobile Menu -->
